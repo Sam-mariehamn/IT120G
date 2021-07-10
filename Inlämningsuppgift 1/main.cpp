3879defff8761b15ccc1595eb24eb33185997c25
Sam-mariehamn <83921244+Sam-mariehamn@users.noreply.github.com>
2021-07-11 00:34:48 +0300

    Update main.cpp

--- Inlämningsuppgift 1/main.cpp
+++ Inlämningsuppgift 1/main.cpp
@@ -12,13 +12,16 @@ int main() {
   int playerTotalScore = 0;
   int computerScore = 0;
   int playerScore = 0;
-  int computerRoll = 0;
-  int playerRoll = 0;
+  int computerRoll1 = 0;
+  int playerRoll1 = 0;
+  int computerRoll2 = 0;
+  int playerRoll2 = 0;
+  int computerRollMax = 0;
+  int playerRollMax = 0;
   srand(time(0));
 
   // While loop som är satt till att köras tills man väljer att avsluta spelet.
   while (true) {
-
     cout << "\nDu har " + to_string(balance) + " kr att spela för. \n\nVälj ett av följande alternativ:\n\n1. Sätta in pengar\n2. Satsa 100 kr\n3. Satsa 300 kr\n4. Satsa 500 kr\n5. Avsluta spelet\n\n>> ";
 
     // While loop som testar om input är ett nummer
@@ -26,8 +29,8 @@ int main() {
       cin.clear();
       cin.ignore(numeric_limits<streamsize>::max(), '\n');
       cout << "Felaktigt val, försök igen.\n\n>> ";
-
     }
+
     // Kollar att man inte sätter in mera än 5000 kr åt gången eller sätter in ett negativt tal.
     // Om man sätter in ett decimaltal klipps decimalerna bort.
     if (choice == 1) {
@@ -39,8 +42,8 @@ int main() {
           cin.clear();
           cin.ignore(numeric_limits<streamsize>::max(), '\n');
           cout << "Felaktigt belopp, försök igen.\n\n>> ";
-
         }
+
         if (deposit > 5000) {
           cout << "Du försökte sätta in för mycket pengar, försök igen. Max 5.000 kr\n>> ";
 
@@ -50,10 +53,11 @@ int main() {
         } else {
           balance += deposit;
           break;
-
         }
       }
+
       continue;
+
     // Kontrollerar att det finns tillräckligt med pengar för satsningen.
     } else if (choice == 2) {
       if (balance < 100) {
@@ -62,8 +66,8 @@ int main() {
 
       } else {
         bet = 100;
-
       }
+
     } else if (choice == 3) {
       if (balance < 300) {
         cout << "Du har inte tillräckligt med pengar." << endl << endl;
@@ -71,8 +75,8 @@ int main() {
 
       } else {
         bet = 300;
-
       }
+
     } else if (choice == 4) {
       if (balance < 500) {
         cout << "Du har inte tillräckligt med pengar." << endl;
@@ -80,7 +84,6 @@ int main() {
 
       } else {
         bet = 500;
-
       }
 
       // Avslutar programmet.
@@ -92,8 +95,8 @@ int main() {
     } else {
       cout << "Felaktigt val, försök igen." << endl;
       continue;
-
     }
+
     cout << "Du har satsat " + to_string(bet) + " kr" << endl << endl;
     
     // Nollställer variablerna inför varje omgång.
@@ -102,19 +105,35 @@ int main() {
 
     // Kastar tärning tills någon har vunnit 2 kast
     while (computerScore < 2 && playerScore < 2) {
-      computerRoll = rand() % 6 + 1;
-      playerRoll = rand() % 6 + 1;
+      computerRoll1 = rand() % 6 + 1;
+      playerRoll1 = rand() % 6 + 1;
+      computerRoll2 = rand() % 6 + 1;
+      playerRoll2 = rand() % 6 + 1;
 
-      cout << "Du kastar " << playerRoll << " och datorn kastar " << computerRoll << "." << endl;
+      cout << "Du kastar " << playerRoll1 << " och " << playerRoll2 << " och datorn kastar " << computerRoll1 << " och " << computerRoll2 << "." << endl;
 
-      // Kontrollerar vem som kastade högst, och om ingen kastade högst blev det lika och ingen får poängen.
-      if (computerRoll > playerRoll) {
+      // Kontrollerar vem som kastade högst, tilldelar poäng, och om ingen kastade högst blev det lika och ingen får poängen.
+      if (computerRoll1 > computerRoll2) {
+        computerRollMax = computerRoll1;
+
+      } else {
+        computerRollMax = computerRoll2;
+
+      }
+      if (playerRoll1 > playerRoll2) {
+        playerRollMax = playerRoll1;
+
+      } else {
+        playerRollMax = playerRoll2;
+
+      }
+      if (computerRollMax > playerRollMax) {
         computerScore += 1;
-        cout << "Datorn vann det här kastet." << endl << endl;
+        cout << "Datorn vann den här omgången." << endl << endl;
 
-      } else if (playerRoll > computerRoll) {
+      } else if (playerRollMax > computerRollMax) {
         playerScore += 1;
-        cout << "Du vann det här kastet." << endl << endl;
+        cout << "Du vann den här omgången." << endl << endl;
 
       } else {
         cout << "Ni kastade lika." << endl << endl;
@@ -123,19 +142,17 @@ int main() {
 
     // Kontrollerar vem som vann omgången, ökar den totala vinsten för vinnaren och ändrar saldot enligt insatsen.
     if (computerScore > playerScore) {
-      cout << "Datorn vann den här omgången!" << endl;
+      cout << "Datorn vann det här spelet!" << endl;
       computerTotalScore += 1;
       balance -= bet;
 
     } else if (playerScore > computerScore) {
-      cout << "Du vann den här omgången!" << endl;
+      cout << "Du vann det här spelet!" << endl;
       playerTotalScore += 1;
       balance += bet;
-
     }
 
     cout << "\nDu har vunnit totalt " + to_string(playerTotalScore) + " gånger och datorn har vunnit " + to_string(computerTotalScore) + " gånger." << endl;
-
   }
 
   return 0;
